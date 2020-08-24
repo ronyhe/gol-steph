11fc7fdf4cca53415f830029b76de06db0b894b1
ronyhe <ronyheruth@wix.com>
2020-08-24 22:36:06 +0300

    Refactor and add tests

--- src/conway.steph.js
+++ src/conway.steph.js
@@ -1,27 +1,24 @@
+const pairWith = flip(pair)
+
+const tripleRange = n => range(n - 1, n + 2)
+
 const makeBoard = cells => {
     const width = head(cells).length
     const height = cells.length
+
     const inBounds = ([x, y]) => y >= 0 && x >= 0 && y < height && x < width
+
     const rawGet = ([x, y]) => cells[y][x]
+
     const rawNeighbors = ([x, y]) => {
-        const rowAbove = [
-            [x - 1, y - 1],
-            [x, y - 1],
-            [x + 1, y - 1]
-        ]
-        const sameRow = [
-            [x - 1, y],
-            [x + 1, y]
-        ]
-        const rowBelow = [
-            [x - 1, y + 1],
-            [x, y + 1],
-            [x + 1, y + 1]
-        ]
-        const allCoordinates = [...rowAbove, ...sameRow, ...rowBelow]
+        const allCoordinates = chain(
+            row => map(pairWith(row), tripleRange(x)),
+            tripleRange(y)
+        )
         const relevant = filter(inBounds, allCoordinates)
-        return map(rawGet, relevant)
+        return map(rawGet, without([[x, y]], relevant))
     }
+
     return {
         get: ifElse(inBounds, rawGet, always(null)),
         getNeighbors: ifElse(inBounds, rawNeighbors, always([]))
